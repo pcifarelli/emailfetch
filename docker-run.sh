d6f8cf3fa608c9ce141ac51308ebb68af3669214
Paul Cifarelli <paul@cifarelli.net>
2017-10-11 15:38:47 +0000

    log to CloudWatch

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,2 @@
-docker run --log-driver=syslog --log-opt syslog-facility=mail --user 5000:5000 --volume=/srv/vmail:/srv/vmail --detach pcifarelli/emailfetch
+#docker run --log-driver=syslog --log-opt syslog-facility=mail --user 5000:5000 --volume=/srv/vmail:/srv/vmail --detach pcifarelli/emailfetch
+docker run --log-driver=awslogs --log-opt awslogs-region=us-east-1 --log-opt awslogs-group=emailfetch --user 5000:5000 --volume=/srv/vmail:/srv/vmail --detach pcifarelli/emailfetch
