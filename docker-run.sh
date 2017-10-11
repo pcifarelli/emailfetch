9dba3e8f0e137f696ef3970d243c7e3ff83718ac
Paul Cifarelli <paul@cifarelli.net>
2017-10-11 15:18:05 +0000

    clearer comments in the Dockerfile for amazonlinux and change logging driver to syslog

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run --user 5000:5000 --volume=/srv/vmail:/srv/vmail --detach pcifarelli/emailfetch
+docker run --log-driver=syslog --log-opt syslog-facility=mail --user 5000:5000 --volume=/srv/vmail:/srv/vmail --detach pcifarelli/emailfetch
